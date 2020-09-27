4822c0bc5833926b12559b61c97abe368c1f06e8
Dae-seong Hwang, Pig-Cola <co.la@kakao.com>
2020-09-28 00:58:05 +0900

    create component CommentList
    CommentList컴포넌트 생성.

--- mohaji/src/reducers/index.js
+++ mohaji/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
-
+import spotReducer from './spot'
+import commentReducer from './comment'
 
 export default combineReducers({
-
+  spotReducer,
+  commentReducer
 });
