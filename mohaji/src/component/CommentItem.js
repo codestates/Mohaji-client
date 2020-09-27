3d88c3a3256d52df82a3142684c7bb53ab377d67
Dae-seong Hwang, Pig-Cola <co.la@kakao.com>
2020-09-27 20:38:46 +0900

    create feature CommentItem
    [Component] CommentItem 제작.

---
+++ mohaji/src/component/CommentItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './CommentItem.css';
+
+class CommentItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='comment'>
+        <div style={{display: 'inline-block', width:'100%'}}>
+          <div className='nickname'>{this.props.nickname}</div>
+          <div className='create_at'>{this.props.create_at}</div>
+        </div>
+        <div className='msg'>{this.props.msg}</div>
+      </div>
+    );
+  }
+}
+
+export default CommentItem;
