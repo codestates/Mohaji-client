9536e8571306eb56d73b24792614a8fae20f4b5c
Dae-seong Hwang, Pig-Cola <co.la@kakao.com>
2020-09-28 22:51:06 +0900

    create component (SpotItem & SpotList)
    제목에 기술된 두 컴포넌트 제작
    react-router-dom을 index.js에 구성시켜
    router기능을 사용할 수 있도록 함.

--- mohaji/src/component/CommentList.js
+++ mohaji/src/component/CommentList.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { loadCommentList, setIsMoreRead } from '../actions';
 import CommentItem from './CommentItem'
+import { Link } from 'react-router-dom';
 
 class CommentList extends React.Component {
   constructor(props) {
@@ -12,14 +13,15 @@ class CommentList extends React.Component {
 
   handleMoreRead() {
     if (this.props.isMoreRead) {
-      this.props.dispatch(loadCommentList(Array(5).fill('').map(()=>({
+      this.props.dispatch(loadCommentList(Array(10).fill('').map(()=>({
         nickname: 'Pig-Cola',
         create_at: new Date().toLocaleString(),
         msg: '안녕하세요'
       }))));
     } else {
       // MoreRead 페이지로 이동
-      this.props.dispatch(setIsMoreRead(true));
+      // <Link to='이동할 Route의 path'/> // MoreRead Component 구축 후 주석 해제
+      this.props.dispatch(setIsMoreRead(true)); // 해당 라인은 MoreRead Component가 구축되면 지울것(주석처리)
     }
   }
 
@@ -28,13 +30,27 @@ class CommentList extends React.Component {
     // api에 따라 달라질 수 있음.
     return (
       <div>
-        {this.props.commentList.map((val,i) => <CommentItem
-          nickname={val.nickname}
-          create_at={val.create_at}
-          msg={val.msg}
-          key={i}
-        />)}
-        <a href='#'><div id='more' onClick={this.handleMoreRead}>더보기</div></a>
+        {this.props.commentList.reduce((acc, val, i) => {
+          if (!this.props.isMoreRead) {
+            if (i < 5) {
+              acc.push(<CommentItem
+                nickname={val.nickname}
+                create_at={val.create_at}
+                msg={val.msg}
+                key={i}
+              />)
+            }
+          } else {
+            acc.push(<CommentItem
+              nickname={val.nickname}
+              create_at={val.create_at}
+              msg={val.msg}
+              key={i}
+            />)
+          }
+          return acc;
+        }, [])}
+        <div id='more' onClick={this.handleMoreRead}>더보기</div>
       </div>
     );
   }
