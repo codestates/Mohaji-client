3624adba57fcf01f453f47b7c293bf1b4e01272b
김현인 <rolenche@gmail.com>
2020-09-29 17:40:23 +0900

    sorry

---
+++ mohaji/src/component/SignIn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class SignIn extends Component {
+    render() {
+        return (
+            <div>
+                <div>모하지 로고 </div>
+                <image src='../../public/모하지.png' />
+            </div>
+        )
+    }
+}
+
